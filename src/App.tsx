88c5d3f15506e9d739d5e71dcfc05788a1ba37b9
Manula <manula.jayabodhi@gmail.com>
2024-08-01 17:57:51 +0530

    Added a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "./pages/Products";
 import AddProduct from "./pages/AddProduct";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/add-product" element={<AddProduct />} />
