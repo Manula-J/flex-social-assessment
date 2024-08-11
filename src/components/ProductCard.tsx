dd8858fdfedf3348d8ad396a24ea20d3b22ca5fa
Manula <manula.jayabodhi@gmail.com>
2024-08-11 10:41:35 +0530

    Styled Product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,8 +18,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       </div>
       <div className="card-body product-card-body">
         <h2 className="card-title">{product.name}</h2>
-        <p className="card-text">{product.description}</p>
-        <p className="card-text">${product.price.toFixed(2)}</p>
+        <p className="card-text description">{product.description}</p>
+        <p className="card-text price">${product.price.toFixed(2)}</p>
         <button type="button" className="btn btn-primary">
           Add to cart
         </button>
