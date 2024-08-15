12e0c0c842c2d90c5a3bf450036ef93cbf913267
Manula <manula.jayabodhi@gmail.com>
2024-08-15 12:52:30 +0530

    Styled temporary product details page

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ProductDetails() {
+  const [itemCount, setItemCount] = useState<number>(0);
+
+  const minusItem = () => {
+    if (itemCount !== 0) {
+      setItemCount(itemCount - 1);
+    }
+  };
+
+  const plusItem = () => {
+    setItemCount(itemCount + 1);
+  };
+
   return (
     <div className="container bg-white">
       <div className="row">
@@ -14,17 +26,31 @@ export default function ProductDetails() {
           </div>
         </div>
         <div className="col-8 p-4">
-          <h2 className="title">Test</h2>
-          <div>Price</div>
-          <div>Description</div>
-          <div className="quantity-selector d-inline-flex">
-            <button>-</button>
-            <div> 0 </div>
-            <button>+</button>
+          <div className="title fs-2 mb-2">Test</div>
+          <div className="price fs-4 font-monospace mb-2">LKR250.99</div>
+          <div className="Description text-break mb-2">Description</div>
+          <div className="quantity-selector d-inline-flex w-25 justify-content-between mt-2 mb-2">
+            <button
+              className="btn btn-primary rounded-circle p-0"
+              onClick={() => minusItem()}
+              style={{ width: "35px", height: "35px" }}
+            >
+              -
+            </button>
+            <div className="count mt-1">{itemCount}</div>
+            <button
+              className="btn btn-primary rounded-circle p-0"
+              onClick={() => plusItem()}
+              style={{ width: "35px", height: "35px" }}
+            >
+              +
+            </button>
           </div>
-          <div>
-            <button d-inline>Buy Now</button>
-            <button>Add to Cart</button>
+          <div className="w-100 mt-3">
+            <button className="d-inline btn btn-warning w-25 me-3">
+              Buy Now
+            </button>
+            <button className="btn btn-secondary w-25">Add to Cart</button>
           </div>
         </div>
       </div>
--- src/pages/ProductInfo.tsx
+++ src/pages/ProductInfo.tsx
@@ -7,8 +7,13 @@ import ProductDetails from "../components/ProductDetails";
 export default function productInfo() {
   return (
     <div className="container">
-      <ProductDetails />
-      {/* <ProductList products={Products} /> */}
+      <div className="row">
+        <div className="col-9">
+          <ProductDetails />
+        </div>
+        <div className="col-3">test2</div>
+        {/* <ProductList products={Products} /> */}
+      </div>
     </div>
   );
 }
